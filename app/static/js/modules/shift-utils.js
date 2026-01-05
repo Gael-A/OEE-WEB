c09fc70e00d457c79ad5d168199f2073caaa3744
Gael Rodarte <gaelrodarte.10@gmail.com>
2026-01-05 12:37:13 -0700

    1.3 Feature correction — 5 minutes plus at start shift range

--- app/static/js/modules/shift-utils.js
+++ app/static/js/modules/shift-utils.js
@@ -2,9 +2,9 @@ export function setShiftByCurrentTime(setShiftValue) {
     const now = new Date();
     const totalMins = now.getHours() * 60 + now.getMinutes();
     let shift = "all";
-    if (totalMins >= 390 && totalMins < 970) shift = "1";
-    if (totalMins >= 980 || totalMins < 30) shift = "2";
-    if (totalMins >= 40 && totalMins < 380) shift = "3";
+    if (totalMins >= 385 && totalMins < 970) shift = "1";
+    if (totalMins >= 975 || totalMins < 30) shift = "2";
+    if (totalMins >= 35 && totalMins < 380) shift = "3";
 
     setShiftValue(shift);
 }
