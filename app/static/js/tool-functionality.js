c86e5e1b411a282abcfb25f16a393bba0b0092e3
Gael Rodarte <gaelrodarte.10@gmail.com>
2026-01-05 12:37:13 -0700

    1.2.1 Feature correction  5 minutes plus at start shift range

--- app/static/js/tool-functionality.js
+++ app/static/js/tool-functionality.js
@@ -127,9 +127,9 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         const totalMins = now.getHours() * 60 + now.getMinutes();
         let shift = "all";
-        if (totalMins >= 390 && totalMins < 970) shift = "1";
-        if (totalMins >= 980 || totalMins < 30) shift = "2";
-        if (totalMins >= 40 && totalMins < 380) shift = "3";
+        if (totalMins >= 385 && totalMins < 970) shift = "1";
+        if (totalMins >= 975 || totalMins < 30) shift = "2";
+        if (totalMins >= 35 && totalMins < 380) shift = "3";
 
         state.currentFormData = {
             pan: panInput.value,
